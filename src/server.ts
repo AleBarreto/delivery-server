d6cfabe6a4a7da2a5c6c0321996339fe297b82eb
Alebarreto <alessandrobarreto50@gmail.com>
2025-12-03 15:23:49 -0400

    up

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { createCourier, setCourierAvailable } from './courierService';
 import { createOrder } from './orderService';
 import { couriers, orders, routes } from './db';
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/health', (_, res) => {
